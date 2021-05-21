049975e7ec21fca6e462c87334d45341c3c5156a
Guadalupe Vazquez <guvadlt@gmail.com>
2021-05-21 10:23:08 -0500

    Se creo la base de conocimientos de estudiantes y se agregaron los estudiantes

---
+++ src/BC_Estudiantes.pl
@@ -0,0 +1,19 @@
+%estudiante(Id,nombre([Apellido,Nombre]),calificaciones([a1,a2,a3,examen])).
+
+estudiante(1001,nombre([allteron,alice]), calificaciones([92,75,100,85])).
+estudiante(1002,nombre([boberson,bob]), calificaciones([50,64,73,66])).
+estudiante(1003,nombre([charleston,charlie]), calificaciones([65,75,85,100])).
+estudiante(1004,nombre([davidson,dave]), calificaciones([10,92,34,87])).
+estudiante(1005,nombre([ellenovitch,ellen]), calificaciones([72,87,82,70])).
+
+%¿Cómo le fue a Alice en la tarea 1?
+
+%¿Cómo le fue a Alice en la tarea 2?
+
+%¿Cómo le fue al estudiante 1002 en el examen?
+
+%¿Quien obtuvo 100 en el extamen?
+
+%¿Alguien mas obtuvo 100 en algo?
+
+%¿Cuál es la calificación promedio de cada estudiante? (asumir la misma ponderación)
